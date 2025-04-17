b621e59b2faf3e6d50971f0185e54baffd9f3660
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2025-04-17 13:41:17 +0800

    delete useless

--- src/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { ShadowBridge } from '.';
-
-describe('ShadowBridge', () => {
-  it('render', async () => {
-    render(<ShadowBridge>foobar</ShadowBridge>);
-    await screen.findByText('foobar');
-  });
-});
